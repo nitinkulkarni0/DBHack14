9d47f4d1210f063715caa60de3fb32476e10f695
DB Hack <DB Hack>
2024-06-10 21:07:20 +0000

    add 5 foobar profiles

--- Untitled Notebook 2024-06-10 11_37_18.py
+++ Untitled Notebook 2024-06-10 11_37_18.py
@@ -25,7 +25,7 @@ print(df_filtered.count()) # 2024 non null experience (they added prior work exp
 display(df_filtered)
 df_filtered.printSchema()
 df_filtered.createOrReplaceTempView("df_filtered")
-spark.sql("create table workspace.default.profiles_bronze as select * from df_filtered")
+# spark.sql("create table workspace.default.profiles_bronze as select * from df_filtered")
 
 
 
@@ -33,3 +33,41 @@ spark.sql("create table workspace.default.profiles_bronze as select * from df_fi
 
 df_bronze = spark.sql("select * from workspace.default.profiles_bronze")
 display(df_bronze)
+
+# COMMAND ----------
+
+# Define the schema for the DataFrame
+table_schema = StructType(
+    [
+        StructField("name", StringType(), True),
+        StructField("experience", schema, True),
+    ]
+)
+
+# Sample data
+data = [
+    ("John", [{"description": "foo bar baz", "title": "data engineer"}]),
+    ("Jane", [{"description": "foo bar baz", "title": "data engineer"}]),
+    (
+        "Tom",
+        [{"description": "foo bar baz", "title": "data engineer"}],
+    ),
+    (
+        "Tom",
+        [{"description": "foo bar baz", "title": "data engineer"}],
+    ),
+    (
+        "Tom",
+        [{"description": "foo bar baz", "title": "data engineer"}],
+    ),
+]
+
+# Create DataFrame
+df = spark.createDataFrame(data, table_schema)
+
+display(df)
+
+
+# COMMAND ----------
+
+
