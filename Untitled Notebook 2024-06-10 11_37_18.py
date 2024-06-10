d85de173d4c7e8fb6e2ce2a8a90c5073591fd398
DB Hack <DB Hack>
2024-06-10 20:44:40 +0000

    add filter logic

--- Untitled Notebook 2024-06-10 11_37_18.py
+++ Untitled Notebook 2024-06-10 11_37_18.py
@@ -1,12 +1,4 @@
 # Databricks notebook source
-# Read the jsonl file and parse the columns
-df = spark.read.json("/Workspace/Repos/Yusuf.Qedan@nike.com/DBHack14/10000_random_us_people_profiles.txt", multiLine=True)
-
-# Display the dataframe
-display(df)
-
-# COMMAND ----------
-
 from pyspark.sql.types import StructType, StructField, ArrayType, StringType
 from pyspark.sql.functions import from_json, get_json_object, col
 
@@ -14,32 +6,30 @@ from pyspark.sql.functions import from_json, get_json_object, col
 
 
 df = spark.sql(
-    "select `name`, experience from linkedin.datasets.linked_in_people_profiles_datasets"
+    "select `name`, experience from workspace.default.profiles_11000"
 )
 schema = ArrayType(
             StructType(
                 [
-                    StructField("duration", StringType()), 
+                    StructField("description", StringType()), 
                     StructField("title", StringType())
                 ]
             )
         )
 
-df = df.select(df.name, from_json(df.experience, schema))
-display(df)
+df = df.select(df.name, from_json(df.experience, schema).alias("experience"))
+df.createOrReplaceTempView("df_raw")
+print(df.count()) # 11000 rows raw data
+df_filtered = spark.sql("select * from df_raw where experience is not null")
+print(df_filtered.count()) # 2024 non null experience (they added prior work exp)
+display(df_filtered)
+df_filtered.printSchema()
+df_filtered.createOrReplaceTempView("df_filtered")
+spark.sql("create table workspace.default.profiles_bronze as select * from df_filtered")
 
 
-# COMMAND ----------
-
-selected_field_df = (
-    df
-    .select(
-        get_json_object(col("experience"), "$[*]['description']").alias("description"),
-        get_json_object(col("experience"), "$[*]['title']").alias("title")
-    )
-)
-display(df)
 
 # COMMAND ----------
 
-
+df_bronze = spark.sql("select * from workspace.default.profiles_bronze")
+display(df_bronze)
